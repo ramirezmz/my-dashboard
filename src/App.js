2b15807247a75d54222f210f4f7804bf59ef40e1
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-03 17:08:28 -0300

    add routes of the project

--- src/App.js
+++ src/App.js
@@ -30,6 +30,46 @@ const App = () => {
               Sidebar w-0
             </div>
           )}
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full
+              ${activeMenu ? 'md:ml-72' : 'flex-2'}
+            `
+          }>
+            <div className='fix md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            Navbar
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path='/' element="ECommerce" />
+              <Route path='/ecommerce' element="ECommerce" />
+
+              {/* Pages */}
+              <Route path='/orders' element="Orders" />
+              <Route path='/employees' element="Employees" />
+              <Route path='/customers' element="Customers" />
+
+              {/* Apps */}
+              <Route path='/kanban' element="Kanban" />
+              <Route path='/editor' element="Editor" />
+              <Route path='/calendar' element="Calendar" />
+              <Route path='/color-picker' element="ColorPicker" />
+
+              {/* Charts */}
+              <Route path='/line' element="Line" />
+              <Route path='/area' element="Area" />
+              <Route path='/bar' element="Bar" />
+              <Route path='/pie' element="Pie" />
+              <Route path='/financial' element="Financial" />
+              <Route path='/color-mapping' element="ColorMapping" />
+              <Route path='/pyramid' element="Pyramid" />
+              <Route path='/stacked' element="Stacked" />
+              
+
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
