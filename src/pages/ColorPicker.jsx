c073139ab2fc788a1316272455b86064d11d4f09
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-03 17:50:23 -0300

    create basic pages to the project

---
+++ src/pages/ColorPicker.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ColorPicker = () => (
+  <div>ColorPicker</div>
+);
+
+export default ColorPicker;
---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Ecommerce = () => (
+  <div>Ecommerce</div>
+);
+
+export default Ecommerce;
