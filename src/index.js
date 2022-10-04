4e8226d1faf170e938ad6aff111eed583667a006
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-04 16:54:19 -0300

    apply context to the root

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './index.css'
-import App from './App'
+import './index.css';
+import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
-)
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'),
+);
