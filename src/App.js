51f0e9ad7075a4c92a7118b4b012fa5055937934
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-10-04 16:54:09 -0300

    apply context to the root

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 // , ThemeSettings, Footer useEffect
 import { Navbar, Sidebar } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorMapping, ColorPicker, Editor, Line } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
